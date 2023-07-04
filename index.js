6fd1d140d7052a35d766147e7f21a3bb968e8603
Manaregr8 <99804338+Manaregr8@users.noreply.github.com>
2023-07-04 09:02:22 +0530

    Update index.js
    done

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const page = (
-   <div>
-       <h1>My awesome website in React</h1>
-       <h3>Reasons I love React</h3>
-       <ol>
-           <li>It's composable</li>
-           <li>It's declarative</li>
-           <li>It's a hireable skill</li>
-           <li>It's actively maintained by skilled people</li>
-       </ol>
-   </div>
-);
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
 
-function App() {
-   return (
-      <div>
-         {page}
-      </div>
-   );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(navbar)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(navbar, document.getElementById("root"))
+// ReactDOM.createRoot(document.getElementById("root")).render(navbar)
